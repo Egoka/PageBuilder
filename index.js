756f29152a30f2a669bdbd3f0bb5d6bcc0982761
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-10-05 18:57:02 +0300

    Basic data has been generated
    model is an object containing the type and the data itself
    $ site is a dom element that will send the received data to the desired div element of the page.

---
+++ index.js
@@ -0,0 +1,11 @@
+const model = [
+    {type: 'title', value:'Hello World from JS'},
+    {type: 'text', value: 'here we go with some text'},
+    {type: 'columns', value: [
+        '1111',
+        '2222',
+        '3333'
+        ]},
+    {type:'image',value: './image/image.png'}
+]
+const $site = document.querySelector('#site')
