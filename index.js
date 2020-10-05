9a400783014273f630cb7df6035c17783fb72f4a
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-10-05 19:02:45 +0300

    Filling out the html page
    Elemento an HTML page is formed from the model object by data type, after which it is filled with data. As a result, using the insertAdjakentNTML method, the resulting html text is substituted in the required place.

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ const model = [
     {type:'image',value: './image/image.png'}
 ]
 const $site = document.querySelector('#site')
+model.forEach(block=> {
+         if (block.type === 'title')  html = title(block)
+    else if (block.type === 'text')   html = text(block)
+    else if (block.type === 'columns')html = columns(block)
+    else if (block.type === 'image')  html = image(block)
+    $site.insertAdjacentHTML('beforeend', html)
+})
