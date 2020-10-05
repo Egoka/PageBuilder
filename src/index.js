1bcc0785e2856e65f8c4ea9577072a7a73ea30e5
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-10-05 22:08:21 +0300

    Connecting the object to the template
    Instead of construction, if is substituted into the function objecttemplate argument argument block.type, resulting in the resulting textHTML.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import './styles/style.css'
 
 const $site = document.querySelector('#site')
 model.forEach(block=> {
-    let html=''
-         if (block.type === 'title')  html = title(block)
-    else if (block.type === 'text')   html = text(block)
-    else if (block.type === 'columns')html = columns(block)
-    else if (block.type === 'image')  html = image(block)
-    $site.insertAdjacentHTML('beforeend', html)
+    const textHTML = templates[block.type]
+    if (textHTML) {
+        $site.insertAdjacentHTML('beforeend', textHTML(block))
+    }
 })
