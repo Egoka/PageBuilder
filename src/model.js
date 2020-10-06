dd7f38c09fed0affd918ee01605c868014c98474
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-10-06 16:40:59 +0300

    Child classes of the Block class
    Instead of one Block class, four child classes TitleBlock, TextBlock, ColumnsBlock, ImageBlock are generated in the blocks.js file.
    The created classes are applied in the model.js file.

--- src/model.js
+++ src/model.js
@@ -1,8 +1,8 @@
 import image from './image/image.png'
-import {Block} from "./Classes/bloks";
+import {TitleBlock, TextBlock, ColumnsBlock, ImageBlock} from "./Classes/blocks";
 
 export const model = [
-    new Block('title', 'Hello World from JS', {
+    new TitleBlock( 'Hello World from JS', {
         tag:'h2',
         styles:{
             'background':'linear-gradient(to top, #dc3545, #a72886)',
@@ -10,7 +10,7 @@ export const model = [
             'text-align':'center',
             'padding':'1.5rem'
         }}),
-    new Block('text', 'here we go with some text', {
+    new TextBlock('here we go with some text', {
         styles: {
             'background':'linear-gradient(to top, #7a86ff, #dc3545)',
             'padding':'1rem',
@@ -19,7 +19,7 @@ export const model = [
             'color': '#89ff22',
         }
     }),
-    new Block('columns', [
+    new ColumnsBlock([
         '-f флаг используется для принудительного выполнения действия',
         '-m флаг используется, когда требуется ввести сообщение без открытия редактора',
         '-i флаг используется для включения интерактивная оболочки',
@@ -33,7 +33,7 @@ export const model = [
             'font-weight':'bold'
         }
     }),
-    new Block('image', image, {
+    new ImageBlock(image, {
         styles: {
             'padding': '2rem 0',
             'display': 'flex',
