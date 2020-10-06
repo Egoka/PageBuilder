4704dd87202787166ea31640fa6f6558a0c56b97
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-10-06 16:57:16 +0300

    toHTML function
    The toHTML() function was created in the parent class Block, and it throws an error about the absence of a function in the child class.
    In the TitleBlock, TextBlock, ColumnsBlock, ImageBlock classes, the toHTML() function executes the whole function from the templates.js file. hence all functionality of templates.js file goes into classes of blocks.js file. The templates.js file has been removed.

--- src/Classes/blocks.js
+++ src/Classes/blocks.js
@@ -1,27 +1,46 @@
+import {dev} from "../utils";
+
 class Block{
-    constructor(type, value, options) {
-        this.type = type
+    constructor(value, options) {
         this.value = value
         this.options = options
+    }
+    toHTML(){
+        throw new Error('Метод не реализован!')
     }
 }
 export class TitleBlock extends Block{
     constructor(value, options) {
-        super('title', value, options)
+        super(value, options)
+    }
+    toHTML() {
+        const {tag='h1',styles} = this.options
+        return dev.row(dev.col(`<${tag}>${this.value}</${tag}>`),dev.css(styles))
     }
 }
 export class TextBlock extends Block{
     constructor(value, options) {
-        super('text', value, options)
+        super(value, options)
+    }
+    toHTML() {
+        return dev.row(dev.col(`<p>${this.value}</p>`), dev.css(this.options.styles))
     }
 }
 export class ColumnsBlock extends Block{
     constructor(value, options) {
-        super('columns', value, options)
+        super(value, options)
+    }
+    toHTML() {
+        const html = this.value.map(item=>dev.col(item)).join('')
+        return dev.row(html, dev.css(this.options.styles))
     }
 }
 export class ImageBlock extends Block{
     constructor(value, options) {
-        super('image', value, options)
+        super(value, options)
+    }
+    toHTML() {
+        const {imageStyles:styl, styles} =this.options
+        return dev.row(`<img src="${this.value}" style="${dev.css(styl)}">`, dev.css(styles))
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import{model} from "./model"
-import{templates} from "./templates"
 import './styles/style.css'
 
 const $site = document.querySelector('#site')
 model.forEach(block=> {
-    const textHTML = templates[block.type]
-    if (textHTML) {
-        $site.insertAdjacentHTML('beforeend', textHTML(block))
-    }
+    $site.insertAdjacentHTML('beforeend', block.toHTML())
 })
