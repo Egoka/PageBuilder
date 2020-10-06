a3fcbc9757f81d806569cb0cffa565ef8eaea1b5
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-10-06 14:37:01 +0300

    Using the styles parameter
    The css () function was applied with a styles argument that was taken from the block.options object.

--- src/templates.js
+++ src/templates.js
@@ -1,16 +1,18 @@
 import{dev} from "./utils";
 
 function title(block){
-    return(dev.row(dev.col(`<h1>${block.value}</h1>`)))
+    const {tag='h1',styles} = block.options
+    return dev.row(dev.col(`<${tag}>${block.value}</${tag}>`),dev.css(styles))
 }
 function text(block){
-    return(dev.row(dev.col(`<p>${block.value}</p>`)))
+    return dev.row(dev.col(`<p>${block.value}</p>`), dev.css(block.options.styles))
 }
 function columns(block){
     const html = block.value.map(item=>dev.col(item)).join('')
-    return(dev.row(html))
+    return dev.row(html, dev.css(block.options.styles))
 }
 function image(block){
-    return(dev.row(`<img src="${block.value}">`))
+    const {imageStyles:styl, styles} =block.options
+    return dev.row(`<img src="${block.value}" style="${dev.css(styl)}">`, dev.css(styles))
 }
 export const templates = {title, text, columns, image}
